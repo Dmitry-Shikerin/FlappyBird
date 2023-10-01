07ba448afb114818b98b7612b8ef8b82d3e285ca
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-01 14:29:25 +0300

    Change

--- Assets/MyProject/Scripts/Enemy/EnemyGenerator.cs
+++ Assets/MyProject/Scripts/Enemy/EnemyGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[System.Serializable]
 public class EnemyGenerator : EnemyPool
 {
     [SerializeField] private GameObject _tamplate;
@@ -11,30 +12,37 @@ public class EnemyGenerator : EnemyPool
     [SerializeField] private float _maxSpawnPositionY;
     [SerializeField] private float _minSpawnPositionY;
 
+     private Coroutine _coroutine;
+    private WaitForSeconds _waitForSeconds;
     private float _elapsedTime = 0;
 
     private void Start()
     {
         Initialize(_tamplate);
+        _waitForSeconds = new WaitForSeconds(_secondsBetweenSpawn);
+        _coroutine = StartCoroutine(GenerateEnemy());
     }
 
-    private void Update()
+    private void OnDisable()
     {
-        _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _secondsBetweenSpawn)
+        StopCoroutine(_coroutine);
+    }
+    
+    private IEnumerator GenerateEnemy()
+    {
+        do
         {
             if (TryGetObject(out GameObject enemy))
             {
-                _elapsedTime = 0;
-
                 float spawnPositionY = Random.Range(_minSpawnPositionY, _maxSpawnPositionY);
                 Vector3 spawnpoint = new Vector3(transform.position.x, spawnPositionY, transform.position.z);
                 enemy.SetActive(true);
                 enemy.transform.position = spawnpoint;
-                
+
                 DisableObjectAbroadScreen();
+
+                yield return _waitForSeconds;
             }
-        }
+        } while (Time.timeScale != 0);
     }
 }
